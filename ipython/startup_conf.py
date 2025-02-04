b7a078d08eb8f0d1c14d566d05f27b9d69bf70ca
Daniel Weber <daniel.weber@cispa.de>
2025-02-04 20:07:02 +0100

    ipython

--- ipython/startup_conf.py
+++ ipython/startup_conf.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python3
 
 def custom_int_repr(n, p, cycle):
-    p.text(f"{n} (0x{n:x}, 0b{n:b})")
+    if n <= 127:
+        # is ascii representable
+        p.text(f"{n} (0x{n:x}, 0b{n:b}, '{n:c}')")
+    else:
+        p.text(f"{n} (0x{n:x}, 0b{n:b})")
 formatter = get_ipython().display_formatter.formatters['text/plain']
 formatter.for_type(int, custom_int_repr)
