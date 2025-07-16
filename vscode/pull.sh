5eb5a4aa6add87edd33b37f44f6a2896f43493d7
Daniel Weber <daniel.weber@cispa.de>
2025-07-16 15:47:58 +0200

    vscode macos stuff

--- vscode/pull.sh
+++ vscode/pull.sh
@@ -1,11 +1,26 @@
 #! /bin/sh
 
+is_macos() {
+  [[ $(uname -s) == "Darwin" ]]
+}
+
 echo "[+] Pulling VSCode JSONs..."
 
 echo "[+] ...extensions.json"
 cp ~/.vscode/extensions/extensions.json .
+
 echo "[+] ...keybindings.json"
-cp ~/.config/Code/User/keybindings.json .
+if is_macos; then
+  cp ~/Library/Application\ Support/Code/User/keybindings.json .
+else
+  # archlinux
+  cp ~/.config/Code/User/keybindings.json .
+fi
+
 echo "[+] ...settings.json"
-cp ~/.config/Code/User/settings.json .
+if is_macos; then
+  cp ~/Library/Application\ Support/Code/User/settings.json .
+else
+  cp ~/.config/Code/User/settings.json .
+fi
 echo "[+] Done."
