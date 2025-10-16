bb1c13ed4b623dd0975bc24ba2963ecba398d4bc
Daniel Weber <daniel.weber@cispa.de>
2025-10-16 14:59:24 +0200

    vim templates

--- vim-templates/=template=.bash
+++ vim-templates/=template=.bash
@@ -1,10 +1,5 @@
 #! /bin/bash
-#
-# %FFILE%
-# Copyright (C) %YEAR% %USER% <%MAIL%>
-#
-# Distributed under terms of the %LICENSE% license.
-#
+set -euo pipefile
 
 
 %HERE%
--- vim-templates/=template=.sh
+++ vim-templates/=template=.sh
@@ -1,10 +1,5 @@
-#! /bin/sh
-#
-# %FFILE%
-# Copyright (C) %YEAR% %USER% <%MAIL%>
-#
-# Distributed under terms of the %LICENSE% license.
-#
+#! /bin/bash
+set -euo pipefail
 
 
 %HERE%
